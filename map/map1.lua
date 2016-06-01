4652cb4ce38f72a2aed2f378daf59add8cbd6664
paly2 <plangrognet@laposte.net>
2016-06-01 12:47:22 +0200

    First commit

---
+++ map/map1.lua
@@ -0,0 +1,7 @@
+map.nodes = {
+{"wall", "wall", "wall", "wall", "wall",  "wall"},
+{"wall", "goal", "wall", "wall", "mario", "wall"},
+{"wall", "none", "box",  "none", "none",  "wall"},
+{"wall", "none", "none", "wall", "wall",  "wall"},
+{"wall", "wall", "wall", "wall", "wall",  "wall"}
+}
